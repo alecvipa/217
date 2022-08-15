4fae7ce96ea7e9317e9857efc18cc5ad757764d9
alecvipa <hebrit_626@hotmail.com>
2022-08-15 10:13:13 -0500

    added changes

--- server.js
+++ server.js
@@ -68,11 +68,21 @@ app.post("/ajax/email", function (request, response) {
 
     
     var htmlBody = `<h2>Correo de contacto</h2><p>Name: ${request.body.name} </p> <p>Phone: ${request.body.number} </p> <p> e-mail: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p> ¿Planea rentar en este mismo mes?: ${request.body.message} </p>`;
+    var secondHtmlBody = `<h2>Gracias por dejar tus datos para: Oficina recién remodelada y amueblada en plaza la antigua</h2><h4>En breve te contactaremos ${request.body.name}<h4/> 
+    <hr> <h5>¿Te gustaría ver el lugar en persona?</h5> <h5>Recuerda que incluímos todos los servicios</h5>
+    <h5>Tendrías un lugar de estacionamiento gratuito y seguridad por medio de portero virtual</h5>`;
+    let customerMail = `${request.body.email}`
     var mail = {
         from: '"Team: Xyncs Web Studio',
         to: 'hebrit_626@hotmail.com',
         subject: '¡Alguien ha dejado sus datos en Oficina 217!',
         html: htmlBody
+    };
+    var secondMail = {
+        from: 'Team: Xyncs Web Studio',
+        to: customerMail,
+        subject: '¡Gracias por registrar tus datos en Oficina 217!',
+        html: secondHtmlBody
     };
     transporter.sendMail(mail, function (err, info) {
         if (err) {
@@ -81,6 +91,13 @@ app.post("/ajax/email", function (request, response) {
             console.log("message sent!");
         };
     });
+    transporter.sendMail(secondMail, function (err, info) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("message sent!");
+        };
+    })
 });
 
 
